2679a4ff9a9d27311fbb5b1010091f87cad91eb7
SwarupaJinne <jinneswarupafall2022@gmail.com>
2023-04-05 21:30:50 -0500

    bonus

--- routes/board.js
+++ routes/board.js
@@ -1,15 +1,9 @@
-var express = require('express');
-var router = express.Router();
+extends layout
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  let query = req.query;
-  console.log(`rows ${query.rows}`);
-  console.log(`cols ${query.cols}`);
-  var rows=Number(query.rows);
-  var cols=Number(query.cols);
-  res.render('board', { title: 'Board Display',query:query });
-
-});
-
-module.exports = router;
+block content
+  h1= title
+  - var image_names = ["shirt1.jpg","shirt2.jpg","shirt3.jpg","shirt4.jpg","shirt5.jpg"]
+  - for(let i=0;i<3;i++){
+     - let random= Math.floor(Math.random()*5)
+     img(src=`/images/${image_names[random]}` alt=`Image No ${random}` height="300px" width="300px")
+  - }
